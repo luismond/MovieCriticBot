7c73ad25fd075d0477e4717fa81ea25a093cc3ca
Luis Mondragón <luismond@gmail.com>
2019-05-09 07:28:45 -0600

    Update moviecriticbot.py

--- moviecriticbot.py
+++ moviecriticbot.py
@@ -1,38 +1,58 @@
 # -*- coding: utf-8 -*-
 from __future__ import absolute_import
 from flask import Flask, render_template, flash, request, Markup
-#from wtforms import Form, TextField, TextAreaField, validators, StringField, SubmitField
 from wtforms import TextField,validators,Form
 from textgenrnn import textgenrnn
 from keras import backend as K
 
-#Load model
+from sklearn.feature_extraction.text import TfidfVectorizer
+vectorizer = TfidfVectorizer(lowercase=True)
+from joblib import load
 
+
+# =============================================================================
+# PREDICT
+# =============================================================================
+
+# English
+vocab_en = open('predict/en/Sentiment_Classifier_en_neural_MLP_vocab.txt','r',encoding='utf8').read().split('\n')
+vectorizer_en = TfidfVectorizer(lowercase=True)
+vectorizer_en.fit(vocab_en)
+clf_en = load('predict/en/Sentiment_Classifier_en_neural_MLP.joblib') 
+
+# Spanish
+vocab_es = open('predict/es/Sentiment_Classifier_es_neural_MLP_vocab.txt','r',encoding='utf8').read().split('\n')
+vectorizer_es = TfidfVectorizer(lowercase=True)
+vectorizer_es.fit(vocab_es)
+clf_es = load('predict/es/Sentiment_Classifier_es_neural_MLP.joblib') 
+
+
+# =============================================================================
+# GENERATE
+# =============================================================================
+
+#Load models
 def generate_phrases(sent,lang):
     #Before prediction
     K.clear_session()
     textgen = textgenrnn(weights_path='models/{}_{}/top_{}_IMDb_sentences_weights_{}.hdf5'.format(sent,lang,sent,lang),
                          vocab_path='models/{}_{}/top_{}_IMDb_sentences_vocab_{}.json'.format(sent,lang,sent,lang),
                          config_path='models/{}_{}/top_{}_IMDb_sentences_config_{}.json'.format(sent,lang,sent,lang))
-   
     #Generate text   
-    # this temperature schedule cycles between 1 very unexpected token, 1 unexpected token, 2 expected tokens, repeat.
-    # changing the temperature schedule can result in wildly different output!    
     temperature = [0.4,0.2]
-    prefix = ''   # if you want each generated text to start with a given seed text
-    x = textgen.generate(temperature=temperature,n=1,
+    prefix = ''
+    phrase = textgen.generate(temperature=temperature,n=1,
                          max_gen_length=150,prefix=prefix,return_as_list=True)
     #After prediction
     K.clear_session()
-    return x
+    return phrase
 
 # App config.
-DEBUG = True
+DEBUG = False
 app = Flask(__name__)
 
 app.config.from_object(__name__)
 app.config['SECRET_KEY'] = '7d441f27d441f27567d441f2b6176a'
-
 
 def get_snippet(lang,sent):
     if sent == 'neg':
@@ -44,36 +64,119 @@ def get_snippet(lang,sent):
     return snippet
 
 
+def get_snippet_pred(sent,name,lang):
+    if sent == 'neg':
+        bot = 'AngryBotFlat-Sm_s.png'
+        if lang == 'en':
+            phrase = 'This review is negative!'
+        if lang == 'es':
+            phrase = '¡Esta opinión es negativa!'
+    if sent == 'pos':
+        bot = 'Robot-60-TopHat-A_s.png'
+        if lang == 'en':
+            phrase = 'This review is positive!'
+        if lang == 'es':
+            phrase = '¡Esta opinión es positiva!'
+    snippet = """<p>{}</p></br><img src="static\{}"></img><div class="phrase_{}">{}</div>""".format(name,bot,sent,phrase)
+    return snippet
+
+
 @app.route("/", methods=['GET', 'POST'])
 
-def contact():
+def home():
     form = Form
     if request.method == 'POST':
-        if request.form.get('English'):
-            if request.form['submit_button'] == 'Negative':
-                snippet = get_snippet('en','neg')
-                flash(Markup(snippet))
-                pass 
-            elif request.form['submit_button'] == 'Positive':
-                snippet = get_snippet('en','pos')            
-                flash(Markup(snippet))
-                pass 
 
-        if request.form.get('Spanish'):
-            if request.form['submit_button'] == 'Negative':
-                snippet = get_snippet('es','neg')
-                flash(Markup(snippet))
-                pass 
-            elif request.form['submit_button'] == 'Positive':
-                snippet = get_snippet('es','pos')
-                flash(Markup(snippet))
-                pass 
-        
-        if request.form.get(None):
-            flash('Please select a language. Movie critic bot is bilingual')
-            pass # unknown
+        if request.form['submit_button'] == 'Negative':
+            snippet = get_snippet('en','neg')
+            flash(Markup(snippet))
+            pass 
+        elif request.form['submit_button'] == 'Positive':
+            snippet = get_snippet('en','pos')            
+            flash(Markup(snippet))
+            pass 
 
-    return render_template('home.html',form=form)
+    return render_template('generate_en.html',form=form)
+
+
+
+@app.route("/en_generate", methods=['GET', 'POST'])
+
+def generate_en():
+    form = Form
+    if request.method == 'POST':
+
+        if request.form['submit_button'] == 'Negative':
+            snippet = get_snippet('en','neg')
+            flash(Markup(snippet))
+            pass 
+        elif request.form['submit_button'] == 'Positive':
+            snippet = get_snippet('en','pos')            
+            flash(Markup(snippet))
+            pass 
+
+    return render_template('generate_en.html',form=form)
+
+@app.route("/es_generate", methods=['GET', 'POST'])
+
+def generate_es():
+    form = Form
+    if request.method == 'POST':
+
+        if request.form['submit_button'] == 'Negativa':
+            snippet = get_snippet('es','neg')
+            flash(Markup(snippet))
+            pass 
+        elif request.form['submit_button'] == 'Positiva':
+            snippet = get_snippet('es','pos')            
+            flash(Markup(snippet))
+            pass 
+
+    return render_template('generate_es.html',form=form)
+
+
+class ReusableForm(Form):
+    name = TextField('word:', validators=[validators.required()])
+
+@app.route("/en_predict", methods=['GET', 'POST'])
+
+def predict_en():
+    form = ReusableForm(request.form)
+    if request.method == 'POST':
+        name=request.form['name']
+        if form.validate():
+            if len(name)>30:
+                pred = clf_en.predict(vectorizer_en.transform([name]))
+                if pred == 0:
+                    snippet = get_snippet_pred('neg',name,'en')
+                    flash(Markup(snippet))
+                if pred == 1:
+                    snippet = get_snippet_pred('pos',name,'en')
+                    flash(Markup(snippet))
+            else:
+                flash('Please enter at least 30 characters')
+
+    return render_template('predict_en.html',form=form)
+
+@app.route("/es_predict", methods=['GET', 'POST'])
+
+def predict_es():
+    form = ReusableForm(request.form)
+    if request.method == 'POST':
+        name=request.form['name']
+        if form.validate():
+            if len(name)>30:
+                pred = clf_en.predict(vectorizer_en.transform([name]))
+                if pred == 0:
+                    snippet = get_snippet_pred('neg',name,'es')
+                    flash(Markup(snippet))
+                if pred == 1:
+                    snippet = get_snippet_pred('pos',name,'es')
+                    flash(Markup(snippet))
+            else:
+                flash('Por favor, escribe por lo menos 30 caracteres')
+
+    return render_template('predict_es.html',form=form)
 
 if __name__ == "__main__":
     app.run(debug=True)
